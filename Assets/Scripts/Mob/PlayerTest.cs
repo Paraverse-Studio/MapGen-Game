4d44eaf6b43bb5740a5f01a9cdb8d82157347030
Abhishek Mohan <abhishekmohan.am@gmail.com>
2021-11-22 14:27:55 -0500

    improve player respawn

--- Assets/Scripts/Mob/PlayerTest.cs
+++ Assets/Scripts/Mob/PlayerTest.cs
@@ -80,7 +80,7 @@ public class PlayerTest : MonoBehaviour
             failingSafePositionCounter += 1f;
             _body.position = lastSafePosition;
 
-            if (failingSafePositionCounter > 1) _body.position = MapGeneration.Instance.CenterPoint + new Vector3(0, 10, 0);
+            if (failingSafePositionCounter > 1) _body.position = MapGeneration.Instance.CenterPoint + new Vector3(0, 3, 0);
         }
 
     }
