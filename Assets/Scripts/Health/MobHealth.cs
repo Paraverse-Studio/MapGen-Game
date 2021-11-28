8dc3e3c33dbf32485fdbb6be7ce3defbf27f67c8
Abhishek Mohan <abhishekmohan.am@gmail.com>
2021-11-28 00:01:32 -0500

    finished health bar take damage increase total etc

--- Assets/Scripts/Health/MobHealth.cs
+++ Assets/Scripts/Health/MobHealth.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using NaughtyAttributes;
 
 public class MobHealth : MonoBehaviour
 {
@@ -11,8 +12,19 @@ public class MobHealth : MonoBehaviour
     public float healthBarHeight = 2.0f;
 
     private GameObject _healthBar;
+    private Transform _healthBarGroup;
 
     private MobController _mobController;
+
+    private int _health;
+    public int Health
+    {
+        get { return _health; }
+        set { _health = Mathf.Clamp(value, 0, _totalHealth); }
+    }
+    [SerializeField]
+    private int _totalHealth;
+
 
     private void Awake()
     {
@@ -24,14 +36,16 @@ public class MobHealth : MonoBehaviour
     void Start()
     {
         CreateHealthBar();
+        _healthBarGroup = _healthBar.transform.GetChild(0);
+        SetTotalHealth(_totalHealth);
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
 
+    }
 
     private void CreateHealthBar()
     {
@@ -42,6 +56,47 @@ public class MobHealth : MonoBehaviour
         _healthBar.transform.SetParent(healthBarFolder);
         FollowTarget2D ft = _healthBar.GetComponent<FollowTarget2D>();
         ft.target = _mobController.Body;
-        ft._offset = new Vector3(0, healthBarHeight, 0);
+        ft._offset = new Vector3(0, healthBarHeight, 0);        
     }
+
+    [Button]
+    public void IncreaseTotalHealth()
+    {
+        _totalHealth++;
+        SetTotalHealth(_totalHealth);
+    }
+    
+    public void SetTotalHealth(int val = 1)
+    {
+        for (int i = _healthBarGroup.childCount-1; i > 0; --i)
+        {
+            Destroy(_healthBarGroup.GetChild(i).gameObject);
+        }
+
+        _totalHealth = val;
+
+        for (int i = 0; i < _totalHealth - 1; ++i)
+        {
+            Instantiate(_healthBarGroup.GetChild(0), _healthBarGroup);
+        }
+        UpdateBarWidthFromHealth();
+    }
+
+
+    public void UpdateBarWidthFromHealth()
+    {
+        int width = (int)Mathf.Min((_totalHealth * Mathf.Max(35 - (_totalHealth * 1.65f), 16f)) + 50, 170+(_totalHealth*6));
+
+        RectTransform rt = _healthBarGroup.GetComponent<RectTransform>();
+        rt.sizeDelta = new Vector2(width, rt.sizeDelta.y);
+    }
+
+
+    public void TakeDamage(int dmg)
+    {
+        Health -= dmg;
+    }
+
+
+
 }
