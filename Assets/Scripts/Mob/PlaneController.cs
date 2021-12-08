4677903179bce8fd708aeccb308cc39fbb594992
Abhishek Mohan <abhishekmohan.am@gmail.com>
2021-12-08 00:18:46 -0500

    sure

--- Assets/Scripts/Mob/PlaneController.cs
+++ Assets/Scripts/Mob/PlaneController.cs
@@ -1,146 +1,41 @@
-using UnityEngine;
 using System.Collections;
-
+using System.Collections.Generic;
+using UnityEngine;
 
 public class PlaneController : MonoBehaviour
 {
-    [Header("Automotion ")]
-    public bool isPlayer = false;
-    public float speed = 6.0f;
-    public Camera camera;
+    public CharacterController controller;
+    public Transform cam;
+    public float speed = 6f;
+    public float turnSmoothTime = 0.1f;
 
-    private CharacterController _characterController;
-    private Renderer _renderer;
-
-    private Vector3 _changeDirection = Vector3.zero;
-    public Vector3 ChangeDirection
-    {
-        get { return _changeDirection; }
-        set { _changeDirection = value; }
-    }
-
-    private Vector3 _moveVector = Vector3.zero;
-    public Vector3 MoveDirection
-    {
-        get { return _moveVector; }
-        set { _moveVector = value; }
-    }
-
-    private Vector3 _finalDirection;
-    public Vector3 FinalDirection => _finalDirection;
-
-    private Vector3 moveVector;
-
-    private Vector3 _lastSafePosition = Vector3.zero;
-    private Transform _body;
-    public Transform Body => _body;
-
-
-    // For disabling player movement, gravity, input, all
-    private bool _active = true;
-    public bool Active => _active;
-
-
-    private Vector2 screenCenter;
-
-    public float lookRateSpeed = 50f;
-    private Vector2 lookInput;
-    private Vector2 mouseDistance;
-
-    private void Awake()
-    {
-        _characterController = GetComponentInChildren<CharacterController>();
-        _renderer = GetComponentInChildren<Renderer>();
-        _body = GetComponentInChildren<CharacterController>().transform;
-    }
-
+    float turnSmoothVelocity;
 
     void Start()
     {
-        if (MapGeneration.Instance && isPlayer) MapGeneration.Instance.OnMapGenerateEnd.AddListener(TeleportPlayer);
-
-        // calculate the correct vertical position
-        float correctHeight = _characterController.center.y + _characterController.skinWidth;
-        // set the controller center vector
-        _characterController.center = new Vector3(0, correctHeight, 0);
-
-        screenCenter.x = Screen.width * 0.5f;
-        screenCenter.y = Screen.height * 0.5f;
+        Cursor.lockState = CursorLockMode.Confined;
     }
-
 
     void Update()
     {
-        // Conditions /////
-        if (!_active) return;
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
+        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
+        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+        //Don't forget to add an isMoving bool inside your Animator
 
-        if (isPlayer)
+        if (direction.magnitude >= 0.1f)
         {
-            GetCameraMovement();
-            GetMovement();
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
-
-        _characterController.Move(_moveVector * Time.deltaTime);
-
-        //_characterController.transform.rotation = Quaternion.Slerp(
-        //    _characterController.transform.rotation,
-        //    Quaternion.LookRotation(new Vector3(camera.transform.forward.x, 0, camera.transform.forward.z)),
-        //    Time.deltaTime * 1f);
-
-        transform.Rotate(-mouseDistance.y * lookRateSpeed * Time.deltaTime, mouseDistance.x * lookRateSpeed * Time.deltaTime, 0f, Space.Self);
-
-    }
-
-
-    public void TurnTo(Vector3 direction, float lerpValue = 100f)
-    {
-        Vector3 _direction = new Vector3(direction.x, 0, direction.z);
-
-        _body.transform.forward = Vector3.RotateTowards(_body.transform.forward, _direction, Time.deltaTime * lerpValue, 0.0f);
-    }
-
-
-    void GetCameraMovement()
-    {
-        lookInput.x = Input.mousePosition.x;
-        lookInput.y = Input.mousePosition.y;
-
-        mouseDistance.x = (lookInput.x - screenCenter.x) / screenCenter.y;
-        mouseDistance.y = (lookInput.y - screenCenter.y) / screenCenter.y;
-
-        mouseDistance = Vector2.ClampMagnitude(mouseDistance, 1f);
-    }
-
-    void GetMovement()
-    {
-
-
-        float moveX = Input.GetAxis("Vertical");
-        float moveZ = Input.GetAxis("Horizontal");
-
-        _moveVector = (transform.forward * moveX * speed) + (transform.forward * moveZ * speed);
-        
-    }
-
-
-    public void TeleportPlayer(Vector3 pos)
-    {
-        _characterController.enabled = false;
-        transform.position = pos;
-        _characterController.enabled = true;
-    }
-
-    public void TeleportPlayer()
-    {
-        _moveVector.y = -1f;
-        TeleportPlayer(MapGeneration.Instance.CenterPointWithY + new Vector3(0, 1f, 0));
-    }
-
-    public void TogglePlayer(bool onOrOff)
-    {
-        _active = onOrOff;
-        _body.gameObject.SetActive(onOrOff);
     }
 
 
