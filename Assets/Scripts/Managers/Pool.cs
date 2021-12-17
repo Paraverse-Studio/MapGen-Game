d76eceee772bfbf1a6e4e92ccb67a1fcec9cb81f
Abhishek Mohan <abhishekmohan.am@gmail.com>
2021-12-17 05:06:02 -0500

    pool change from list to 2d array

--- Assets/Scripts/Managers/Pool.cs
+++ Assets/Scripts/Managers/Pool.cs
@@ -26,6 +26,8 @@ public class Pool : MonoBehaviour
     public int totalSpawned = 0;
     public int totalProgress = 0;
 
+    private int maxSize = 1;
+
     public int delayAfterEverySet = 1000;
     private int delaySetCounter = 0;
 
@@ -35,7 +37,8 @@ public class Pool : MonoBehaviour
     public List<PoolItem> itemsToPool;
 
     // list that holds all pooledobject lists
-    public List<GameObject> pooledObjects;
+    //public List<GameObject> pooledObjects;
+    public GameObject[,] pooledObjects;
 
     [Space(20)]
     public UnityEvent OnPoolCreateStart = new UnityEvent();
@@ -59,21 +62,17 @@ public class Pool : MonoBehaviour
         // Get total number of items to spawn (this is only for the loading bar to know total progress)
         totalSpawned = 0;
         totalProgress = 0;
+
         for (int i = 0; i < itemsToPool.Count; ++i)
         {
-            if (itemsToPool[i] == null) continue;
-            if (itemsToPool[i].objectToPool == null) continue;
             if (itemsToPool[i].doSpawn == false) continue;
 
-            PoolIndex PI = itemsToPool[i].objectToPool.GetComponent<PoolIndex>();
-            if (null == PI)
-            {
-                itemsToPool[i].objectToPool.AddComponent<PoolIndex>();
-            }
-            PI.id = i;
+            if (itemsToPool[i].amount > maxSize) maxSize = itemsToPool[i].amount;            
 
             totalProgress += itemsToPool[i].amount;
         }
+
+        pooledObjects = new GameObject[itemsToPool.Count, maxSize];
 
         StartCoroutine(EAwake());
     }
@@ -82,15 +81,12 @@ public class Pool : MonoBehaviour
     IEnumerator EAwake()
     {
         OnPoolCreateStart?.Invoke();
-        yield return null;
 
-        pooledObjects = new List<GameObject>(); //list that holds all pooleable objects
+        yield return null;
 
         int size = itemsToPool.Count;
         for (int x = size - 1; x >= 0; --x) //for each pooleable object
         {
-            if (itemsToPool[x] == null) continue;
-            if (itemsToPool[x].objectToPool == null) continue;
             if (itemsToPool[x].doSpawn == false) continue;
 
             GameObject go = new GameObject(); go.name = "[Object Pool - " + itemsToPool[x].objectToPool.name + "]";
@@ -99,15 +95,24 @@ public class Pool : MonoBehaviour
 
             itemsToPool[x].parentObj = go;
 
+            // Create a reference of the prefab object from which to clone the thousands from
+            // so we can add temporary scripts to it, and the pooled ones will auto-have them
+            GameObject clonedPrefab = Instantiate(itemsToPool[x].objectToPool);
+            UtilityFunctions.UpdateLODlevels(clonedPrefab.transform);
+            PoolIndex PI = clonedPrefab.GetComponent<PoolIndex>();
+            if (null == PI)
+            {
+                PI = clonedPrefab.AddComponent<PoolIndex>();
+            }
+            PI.id = x;
+
             for (int i = 0; i < itemsToPool[x].amount; i++)
             {
-                GameObject obj = (GameObject)Instantiate(itemsToPool[x].objectToPool);
+                GameObject obj = Instantiate(clonedPrefab);
                 
-                obj.transform.position = Vector3.zero;
-                obj.transform.rotation = Quaternion.identity;
-                obj.transform.parent = itemsToPool[x].parentObj.transform; obj.SetActive(false); pooledObjects.Add(obj);
-
-                UtilityFunctions.UpdateLODlevels(obj.transform);
+                //obj.transform.position = Vector3.zero;
+                //obj.transform.rotation = Quaternion.identity;
+                obj.transform.parent = itemsToPool[x].parentObj.transform; obj.SetActive(false); pooledObjects[x, i] = obj;
 
                 // Progress bar stuff (purely)
                 totalSpawned += 1;
@@ -118,11 +123,11 @@ public class Pool : MonoBehaviour
                     delaySetCounter = 0;
                     OnProgressChange?.Invoke(totalSpawned, totalProgress);
                     OnProgressChangeText?.Invoke("Pooling asset: " + itemsToPool[x].objectToPool.name);
-                    //yield return null;
                 }
                 ///////////////////////////////
 
             }
+            DestroyImmediate(clonedPrefab);
         }
 
         OnProgressChange?.Invoke(totalSpawned, totalProgress);
@@ -133,65 +138,62 @@ public class Pool : MonoBehaviour
     }
 
     //  OUTDATED - used to use string comparison
-    public GameObject Instantiate_OLD(string nom, Vector3 position, Quaternion rotation, bool usePoolsFolder = true, bool ignoreActive = false)
-    {
-        // 1.0  First, search for unused item in pool to re-use it
-        for (int i = 0; i < pooledObjects.Count; i++)
-        {
-            if (pooledObjects[i] != null &&
-                (ignoreActive || (!pooledObjects[i].activeInHierarchy && !pooledObjects[i].activeSelf)) &&
-                pooledObjects[i].name.Contains(nom))
-            {
-                pooledObjects[i].gameObject.transform.position = position;
-                pooledObjects[i].gameObject.transform.rotation = rotation;
-                pooledObjects[i].SetActive(true);
+    //public GameObject Instantiate_OLD(string nom, Vector3 position, Quaternion rotation, bool usePoolsFolder = true, bool ignoreActive = false)
+    //{
+    //    // 1.0  First, search for unused item in pool to re-use it
+    //    for (int i = 0; i < pooledObjects.Count; i++)
+    //    {
+    //        if (pooledObjects[i] != null &&
+    //            (ignoreActive || (!pooledObjects[i].activeInHierarchy && !pooledObjects[i].activeSelf)) &&
+    //            pooledObjects[i].name.Contains(nom))
+    //        {
+    //            pooledObjects[i].gameObject.transform.position = position;
+    //            pooledObjects[i].gameObject.transform.rotation = rotation;
+    //            pooledObjects[i].SetActive(true);
 
-                return pooledObjects[i];
-            }
-        }
+    //            return pooledObjects[i];
+    //        }
+    //    }
 
-        // 2.0 Being here means we were out of amount, so we search for that PoolItem through list and expand it
-        return MoreNeeded(nom, usePoolsFolder);
-    }
+    //    // 2.0 Being here means we were out of amount, so we search for that PoolItem through list and expand it
+    //    return MoreNeeded(nom, usePoolsFolder);
+    //}
 
     public GameObject Instantiate(int id, Vector3 position, Quaternion rotation, bool usePoolsFolder = true, bool ignoreActive = false)
     {
         // 1.0  First, search for unused item in pool to re-use it
-        for (int i = 0; i < pooledObjects.Count; i++)
+        for (int i = 0; i < maxSize; i++)
         {
-            if (pooledObjects[i] != null &&
-                (ignoreActive || (!pooledObjects[i].activeInHierarchy && !pooledObjects[i].activeSelf)) &&
-                pooledObjects[i].GetComponent<PoolIndex>().id == id
-                )
+            if (pooledObjects[id, i] != null &&
+                (ignoreActive || (!pooledObjects[id, i].activeInHierarchy && !pooledObjects[id, i].activeSelf)) )
             {
-                pooledObjects[i].gameObject.transform.position = position;
-                pooledObjects[i].gameObject.transform.rotation = rotation;
-                pooledObjects[i].SetActive(true);
+                pooledObjects[id, i].gameObject.transform.position = position;
+                pooledObjects[id, i].gameObject.transform.rotation = rotation;
+                pooledObjects[id, i].SetActive(true);
 
-                return pooledObjects[i];
+                return pooledObjects[id, i];
             }
         }
 
-        // 2.0 Being here means we were out of amount, so we search for that PoolItem through list and expand it
         return null; // MoreNeeded(nom, usePoolsFolder);
     }
 
-    private GameObject MoreNeeded(string nom, bool usePoolFolder = true)
-    {
-        Debug.Log("System: More amount of " + nom + " was needed than pooler currently holds.");
-        foreach (PoolItem item in itemsToPool)
-        {
-            if (item.objectToPool.name.Contains(nom) && item.canExpand)
-            {
-                GameObject obj = (GameObject)Instantiate(item.objectToPool);
-                obj.transform.position = Vector3.zero;
-                obj.transform.rotation = Quaternion.identity;
-                if (usePoolFolder) obj.transform.SetParent(item.parentObj.transform);
-                obj.SetActive(true); pooledObjects.Add(obj); return obj;
-            }
-        }
-        return null;
-    }
+    //private GameObject MoreNeeded(string nom, bool usePoolFolder = true)
+    //{
+    //    Debug.Log("System: More amount of " + nom + " was needed than pooler currently holds.");
+    //    foreach (PoolItem item in itemsToPool)
+    //    {
+    //        if (item.objectToPool.name.Contains(nom) && item.canExpand)
+    //        {
+    //            GameObject obj = (GameObject)Instantiate(item.objectToPool);
+    //            obj.transform.position = Vector3.zero;
+    //            obj.transform.rotation = Quaternion.identity;
+    //            if (usePoolFolder) obj.transform.SetParent(item.parentObj.transform);
+    //            obj.SetActive(true); pooledObjects.Add(obj); return obj;
+    //        }
+    //    }
+    //    return null;
+    //}
 
 
     // END OF FILE //////////////
