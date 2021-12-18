f4e865ae7e7f2988d24b2d4b3d7f4f745117f828
Abhishek Mohan <abhishekmohan.am@gmail.com>
2021-12-18 03:27:45 -0500

    perfect DOF settings, stop adjusting this

--- Assets/Scripts/Utlities/UtilityFunctions.cs
+++ Assets/Scripts/Utlities/UtilityFunctions.cs
@@ -28,7 +28,7 @@ public static class UtilityFunctions
             int index = 0;
             for (int i = size - 1; i >= 0; --i)
             {
-                lods[i].screenRelativeTransitionHeight = 0.0175f + (0.025f * index);
+                lods[i].screenRelativeTransitionHeight = 0.015f + (0.020f * index);
                 index++;
             }
             
