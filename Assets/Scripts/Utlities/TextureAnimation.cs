4c02697286c6a888eb6d39f6b9d686bd864b1614
Abhishek Mohan <abhishekmohan.am@gmail.com>
2021-12-21 00:16:54 -0500

    adjusted bloom vs lighting values to prevent dirt block from shining

--- Assets/Scripts/Utlities/TextureAnimation.cs
+++ Assets/Scripts/Utlities/TextureAnimation.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class TextureAnimation : MonoBehaviour
 {
+    public bool useCookieTexture;
+
+    private Light lighting;
+    private Texture cookieTexture;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        lighting = GetComponent<Light>();
+        if (!lighting ) { Debug.Log("No Light component found on this object."); enabled = false; }
+
+        cookieTexture = lighting.cookie;
+        lighting.cookie = useCookieTexture ? cookieTexture : null;
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    //void Update()
+    //{
         
-    }
+    //}
+
+
 }
