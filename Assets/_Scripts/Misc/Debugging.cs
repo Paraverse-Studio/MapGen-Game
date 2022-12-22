2cb1e3d458824ca7df84eac656a8c4f3a14e4327
Abhishek Mohan <abhishekmohan.am@gmail.com>
2022-12-22 13:16:58 -0500

    added testing

---
+++ Assets/_Scripts/Misc/Debugging.cs
@@ -0,0 +1,30 @@
+using Paraverse.Mob.Stats;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Debugging : MonoBehaviour
+{
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Heal All
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            MobStats[] stats = FindObjectsOfType<MobStats>();
+
+            foreach (MobStats s in stats)
+            {
+                s.GetComponentInChildren<MobStats>().SetFullHealth();
+            }
+        }
+
+
+
+
+    }
+
+
+}
