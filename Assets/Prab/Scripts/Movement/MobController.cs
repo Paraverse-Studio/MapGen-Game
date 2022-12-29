b590aea6076bbbab9ec1518da00fa808cc0944ed
Prabdeep Dhaliwal <prab.dhaliwal95@gmail.com>
2022-12-29 15:26:23 -0500

    Fixed _isFalling

--- Assets/Prab/Scripts/Movement/MobController.cs
+++ Assets/Prab/Scripts/Movement/MobController.cs
@@ -500,14 +500,16 @@ namespace Paraverse.Mob.Controller
         float disFromStartPos;
         private void KnockbackHandler()
         {
-            if (null != activeKnockBackEffect)
+            if (null != activeKnockBackEffect || _isStaggered)
             {
                 _isStaggered = true;
+
+                activeKnockBackEffect.maxKnockbackDuration -= Time.deltaTime;
+                disFromStartPos = ParaverseHelper.GetDistance(activeKnockBackEffect.startPos, transform.position);
 
                 // Ensures mob falls when off platform
                 if (CheckFall())
                 {
-                    controller.radius = 0f; 
                     nav.enabled = false;
                     knockbackDir.y = GlobalValues.GravityForce;
                     Vector3 fallDir = new Vector3(knockbackDir.x * activeKnockBackEffect.knockForce, knockbackDir.y * fallForce, knockbackDir.z * activeKnockBackEffect.knockForce);
@@ -520,17 +522,13 @@ namespace Paraverse.Mob.Controller
 
                     return;
                 }
+                else if (disFromStartPos >= activeKnockBackEffect.maxKnockbackRange || activeKnockBackEffect.maxKnockbackDuration <= 0)
+                {
+                    CleanseStagger();
+                }
 
                 // Moves the mob in the move direction
                 controller.Move(knockbackDir * activeKnockBackEffect.knockForce * Time.deltaTime);
-
-                activeKnockBackEffect.maxKnockbackDuration -= Time.deltaTime;
-                disFromStartPos = ParaverseHelper.GetDistance(activeKnockBackEffect.startPos, transform.position);
-                if (disFromStartPos >= activeKnockBackEffect.maxKnockbackRange || activeKnockBackEffect.maxKnockbackDuration <= 0)
-                {
-                    _isStaggered = false;
-                    activeKnockBackEffect = null;
-                }
             }
         }
 
@@ -539,8 +537,16 @@ namespace Paraverse.Mob.Controller
             Vector3 origin = transform.position;
             Vector3 dir = -transform.up;
 
-            Debug.DrawRay(origin, dir * checkFallRange, Color.red);
-            if (Physics.Raycast(origin, dir * checkFallRange, checkFallRange))
+            Vector3 topOrigin = origin + new Vector3(0f, 0f, nav.radius);
+            Vector3 leftOrigin = origin + new Vector3(-nav.radius, 0f, 0f);
+            Vector3 rightOrigin = origin + new Vector3(nav.radius, 0f, 0f);
+
+            Debug.DrawRay(topOrigin, dir * checkFallRange, Color.red);
+            Debug.DrawRay(leftOrigin, dir * checkFallRange, Color.red);
+            Debug.DrawRay(rightOrigin, dir * checkFallRange, Color.red);
+            if (Physics.Raycast(topOrigin, dir * checkFallRange, checkFallRange) &&
+            (Physics.Raycast(leftOrigin, dir * checkFallRange, checkFallRange) &&
+            (Physics.Raycast(rightOrigin, dir * checkFallRange, checkFallRange))))
             {
             }
             else
@@ -581,6 +587,7 @@ namespace Paraverse.Mob.Controller
         {
             if (CheckFall()) return;
             _isStaggered = false;
+            activeKnockBackEffect = null;
         }
         #endregion
 
