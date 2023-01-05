23dc3652eccd431892b6b39fbbc706b005392417
Prabdeep Dhaliwal <prab.dhaliwal95@gmail.com>
2023-01-05 13:18:49 -0500

    Updated Satyr skill and basic attacks animations

--- Assets/Prab/Scripts/Combat/Skills/Satyr Skills/DualComboSkill.cs
+++ Assets/Prab/Scripts/Combat/Skills/Satyr Skills/DualComboSkill.cs
@@ -64,6 +64,10 @@ public class DualComboSkill : MobSkill, IMobSkill
         {
             Execute();
         }
+
+        if (anim.GetBool(StringData.IsUsingSkill) == false)
+            skillOn = false;
+
         CooldownHandler();
     }
 
