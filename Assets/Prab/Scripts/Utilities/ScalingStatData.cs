c07ad96462298605c8a04e79b6cce1bb97ee2870
Abhishek Mohan <abhishekmohan.am@gmail.com>
2023-01-31 16:30:32 -0500

    health scaling in scalingdata

--- Assets/Prab/Scripts/Utilities/ScalingStatData.cs
+++ Assets/Prab/Scripts/Utilities/ScalingStatData.cs
@@ -10,12 +10,15 @@ public class ScalingStatData
     public float attackScaling = 1;
     [Range(0, 3)]
     public float abilityScaling = 0;
+    [Range(0, 3)]
+    public float healthScaling = 0;
 
     public float FinalValue(Paraverse.Mob.Stats.MobStats stats)
     {
         return  flatPower + 
                 (stats.AttackDamage.FinalValue * attackScaling) + 
-                (stats.AbilityPower.FinalValue * abilityScaling);
+                (stats.AbilityPower.FinalValue * abilityScaling) +
+                (stats.MaxHealth.FinalValue * healthScaling);
     }
 
 }
