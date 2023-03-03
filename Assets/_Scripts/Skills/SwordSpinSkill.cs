e31f956ee48938dc4bde809bf07f4eee62683a6b
Abhishek Mohan <abhishekmohan.am@gmail.com>
2023-03-03 15:17:44 -0500

    basics of sword spin skill

--- Assets/_Scripts/Skills/SwordSpinSkill.cs
+++ Assets/_Scripts/Skills/SwordSpinSkill.cs
@@ -6,27 +6,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DashSkill : MobSkill, IMobSkill
+public class SwordSpinSkill : MobSkill, IMobSkill
 {
     #region variables
-    [Header("Dash Skill:")]
+    [Header("Sword Spin Skill:")]
     [SerializeField] private GameObject VFX;
     [SerializeField] private GameObject[] VFXObjects;
-    [SerializeField] private Vector3 thrustForce;
-    [SerializeField] private Vector3 jumpForce;
     [SerializeField] private Vector3 colliderSize;
 
     private Transform _userWeapon = null;
     private GameObject _VFX = null;
     private StatModifier _buff = null;
 
-    private float _originalJumpGravity = 0;
-    private Vector3 _forces = Vector3.zero;
-    private float _resistance = 40f;
-    private bool _thrustStarted = false;
     private CharacterController _controller;
     private PlayerController _player;
-    
+
     #endregion
 
     public override void SkillUpdate()
@@ -37,15 +31,6 @@ public class DashSkill : MobSkill, IMobSkill
         {
             DisableSkill();
             return;
-        }
-
-        if (_controller)
-        {
-            _controller.Move(new Vector3(_forces.x, _forces.y, _forces.z) * Time.deltaTime);
-
-            float y = _forces.y; _forces.y = 0;
-            _forces = Vector3.MoveTowards(_forces, Vector3.zero, _resistance * Time.deltaTime);
-            _forces.y = Mathf.MoveTowards(y, 0, 17f * Time.deltaTime);
         }
     }
 
@@ -58,24 +43,12 @@ public class DashSkill : MobSkill, IMobSkill
         if (null == _player) _player = mob.GetComponent<PlayerController>();
 
         _userWeapon = attackColliderGO.transform.parent;
-        if (0 == _originalJumpGravity) _originalJumpGravity = _player.JumpGravity;
-        _player.JumpGravity = _originalJumpGravity / 3f;
-        _thrustStarted = false;
-        _forces = Vector3.zero;
 
         // GLOWY PARTICLES
         if (null == _VFX && null != VFX) _VFX = Instantiate(VFX, _userWeapon);
         ToggleParticleSystem(turnParticlesOn: true);
 
-        // BUFF OF THE DAMAGE
-        if (null == _buff)
-        {
-            _buff = new StatModifier(GetPowerAmount());
-            stats.AttackDamage.AddMod(_buff);
-        }
-
-        _forces += jumpForce;
-        Physics.IgnoreLayerCollision(14, 15, true);
+        AddSpin();
     }
 
     protected override void DisableSkill()
@@ -88,41 +61,41 @@ public class DashSkill : MobSkill, IMobSkill
         }
 
         if (null != _buff)
-        {        
+        {
             stats.AttackDamage.RemoveMod(_buff);
             _buff = null;
 
-            Physics.IgnoreLayerCollision(14, 15, false);
-            _player.JumpGravity = _originalJumpGravity;
-
             Vector3 scale = attackColliderGO.transform.localScale;
             attackColliderGO.transform.localScale = Vector3.one;
+
+            attackColliderGO.SetActive(false);
         }
     }
 
     public override void SubscribeAnimationEventListeners()
     {
         base.SubscribeAnimationEventListeners();
-        mob.OnChargeSkillOneEvent += AddThrust;
+        mob.OnChargeSkillOneEvent += AddSpin;
     }
 
     public override void UnsubscribeAnimationEventListeners()
     {
         base.UnsubscribeAnimationEventListeners();
-        mob.OnChargeSkillOneEvent -= AddThrust;
+        mob.OnChargeSkillOneEvent -= AddSpin;
     }
 
-    private void AddThrust()
+    private void AddSpin()
     {
-        if (_thrustStarted) return;
-
-        _thrustStarted = true;
-        Vector3 direction = transform.forward;
-        if (mob.Target) direction = (ParaverseHelper.GetPositionXZ(mob.Target.transform.position - mob.transform.position)).normalized;
-        _forces += new Vector3(direction.x * thrustForce.x, direction.y * thrustForce.y, direction.z * thrustForce.z);
+        attackColliderGO.SetActive(true);
 
         Vector3 scale = attackColliderGO.transform.localScale;
         attackColliderGO.transform.localScale = new Vector3(scale.x + colliderSize.x, scale.y + colliderSize.y, scale.z + colliderSize.z);
+
+        if (null == _buff)
+        {
+            _buff = new StatModifier(GetPowerAmount());
+            stats.AttackDamage.AddMod(_buff);
+        }
     }
 
     private float GetPowerAmount()
@@ -143,7 +116,7 @@ public class DashSkill : MobSkill, IMobSkill
         }
         if (null != VFXObjects && true == turnParticlesOn)
         {
-            foreach(GameObject obj in VFXObjects)
+            foreach (GameObject obj in VFXObjects)
             {
                 if (null != obj) Instantiate(obj, _userWeapon);
             }
