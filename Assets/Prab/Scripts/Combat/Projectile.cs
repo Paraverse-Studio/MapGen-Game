a6e85624c54c1041367ac04bd760ab8c8d9f3eae
Prabdeep Dhaliwal <prab.dhaliwal95@gmail.com>
2023-12-15 11:37:33 -0500

    Updated Projectiles to fire at a straight line, rather than following target

--- Assets/Prab/Scripts/Combat/Projectile.cs
+++ Assets/Prab/Scripts/Combat/Projectile.cs
@@ -77,7 +77,7 @@ namespace Paraverse
 
             if (stationary == false)
             {
-                transform.position += transform.forward * speed * Time.deltaTime;
+                transform.position += new Vector3(transform.forward.x, 0, transform.forward.z) * speed * Time.deltaTime;
                 if (decreaseByLerp > 0) speed = Mathf.Lerp(speed, 0, decreaseByLerp * Time.deltaTime);
             }
         }
