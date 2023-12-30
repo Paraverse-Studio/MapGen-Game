e7e10199252f74aefa429c84886c9a6fd01994db
Prabdeep Dhaliwal <prab.dhaliwal95@gmail.com>
2023-12-29 23:11:30 -0500

    Dont apply effects to OnDeath() method for player

--- Assets/Prab/Scripts/Stats/EffectMods/MobEffect.cs
+++ Assets/Prab/Scripts/Stats/EffectMods/MobEffect.cs
@@ -9,90 +9,91 @@ using UnityEngine;
 
 public abstract class MobEffect : MonoBehaviour
 {
-    [SerializeField]
-    protected string EffectName = "Mob Effect";
-    protected MobStats _stats;
-    protected PlayerCombat _combat;
+  [SerializeField]
+  protected string EffectName = "Mob Effect";
+  protected MobStats _stats;
+  protected PlayerCombat _combat;
 
-    [SerializeField]
-    protected string targetTag = StringData.EnemyTag;
-    public int ID { get { return _ID; } set { _ID = value; } }
-    [SerializeField, Tooltip("Effect ID")]
-    protected int _ID = -1;
+  [SerializeField]
+  protected string targetTag = StringData.EnemyTag;
+  public int ID { get { return _ID; } set { _ID = value; } }
+  [SerializeField, Tooltip("Effect ID")]
+  protected int _ID = -1;
 
-    protected List<GameObject> hitTargets = new List<GameObject>();
+  protected List<GameObject> hitTargets = new List<GameObject>();
 
-    public ScalingStatData scalingStatData;
+  public ScalingStatData scalingStatData;
 
-    [SerializeField]
-    protected GameObject effectFX;
-    [SerializeField]
-    protected GameObject hitFX;
+  [SerializeField]
+  protected GameObject effectFX;
+  [SerializeField]
+  protected GameObject hitFX;
 
-    public bool IsActive { get { return isActive; } }
-    protected bool isActive = false;
-    protected GameObject _FX;
+  public bool IsActive { get { return isActive; } }
+  protected bool isActive = false;
+  protected GameObject _FX;
 
-    public virtual void ActivateEffect(MobStats stats)
+  public virtual void ActivateEffect(MobStats stats)
+  {
+    _stats = stats;
+    _combat = _stats.GetComponent<PlayerCombat>();
+    isActive = true;
+  }
+
+  public virtual void DeactivateEffect()
+  {
+    isActive = false;
+
+    // Destroys instantiated FX
+    if (_FX) Destroy(_FX.gameObject);
+  }
+
+  public virtual void AddSubscribersToSkillEvents(Damage col)
+  {
+
+  }
+
+  public virtual void RemoveSubscribersToSkillEvents(Damage col)
+  {
+
+  }
+
+  public virtual void OnSkillChangeApplyEffect()
+  {
+
+  }
+
+  public virtual void OnEnemyDeathApplyEffect(MobController enemy)
+  {
+    // dont apply effect to players OnDeath() method
+    if (enemy.tag.Equals(StringData.PlayerTag)) return;
+  }
+
+  /// <summary>
+  /// useCustomDamage needs to be set to true on AttackCollider.cs inorder to apply this.
+  /// </summary>
+  public virtual float ApplyCustomDamage(IMobController controller)
+  {
+    float totalDmg = scalingStatData.FinalValueWithBoosts(_stats);
+
+    controller.Stats.UpdateCurrentHealth(-Mathf.CeilToInt(totalDmg));
+    return totalDmg;
+  }
+
+  protected virtual void DamageLogic(Collider other)
+  {
+    hitTargets.Add(other.gameObject);
+
+    // Enemy-related logic
+    if (other.TryGetComponent(out IMobController controller))
     {
-        _stats = stats;
-        _combat = _stats.GetComponent<PlayerCombat>();
-        isActive = true;
+      // Apply damage
+      ApplyCustomDamage(controller);
     }
 
-    public virtual void DeactivateEffect()
-    {
-        isActive = false;
+    // General VFX logic
+    if (hitFX) Instantiate(hitFX, other.transform.position + new Vector3(0, 0.5f, 0), Quaternion.identity);
 
-        // Destroys instantiated FX
-        if (_FX) Destroy(_FX.gameObject);
-    }
-
-    public virtual void AddSubscribersToSkillEvents(Damage col)
-    {
-
-    }
-
-    public virtual void RemoveSubscribersToSkillEvents(Damage col)
-    {
-
-    }
-
-    public virtual void OnSkillChangeApplyEffect()
-    {
-
-    }
-
-    public virtual void OnEnemyDeathApplyEffect(MobController enemy)
-    {
-
-    }
-
-    /// <summary>
-    /// useCustomDamage needs to be set to true on AttackCollider.cs inorder to apply this.
-    /// </summary>
-    public virtual float ApplyCustomDamage(IMobController controller)
-    {
-        float totalDmg = scalingStatData.FinalValueWithBoosts(_stats);
-
-        controller.Stats.UpdateCurrentHealth(-Mathf.CeilToInt(totalDmg));
-        return totalDmg;
-    }
-
-    protected virtual void DamageLogic(Collider other)
-    {
-        hitTargets.Add(other.gameObject);
-
-        // Enemy-related logic
-        if (other.TryGetComponent(out IMobController controller))
-        {
-            // Apply damage
-            ApplyCustomDamage(controller);
-        }
-
-        // General VFX logic
-        if (hitFX) Instantiate(hitFX, other.transform.position + new Vector3(0, 0.5f, 0), Quaternion.identity);
-
-        Debug.Log(other.name + " took " + _stats.AttackDamage.FinalValue + " points of damage.");
-    }
+    Debug.Log(other.name + " took " + _stats.AttackDamage.FinalValue + " points of damage.");
+  }
 }
--- Assets/Prab/Scripts/Stats/EffectMods/ReaperKillEffect.cs
+++ Assets/Prab/Scripts/Stats/EffectMods/ReaperKillEffect.cs
@@ -5,35 +5,35 @@ using UnityEngine;
 
 public class ReaperKillEffect : MobEffect
 {
-    [SerializeField]
-    protected int healAmount = 3;
+  [SerializeField]
+  protected int healAmount = 3;
 
-    public override void ActivateEffect(MobStats stats)
+  public override void ActivateEffect(MobStats stats)
+  {
+    base.ActivateEffect(stats);
+    foreach (MobController enemy in EnemiesManager.Instance.Enemies)
     {
-        base.ActivateEffect(stats);
-        foreach (MobController enemy in EnemiesManager.Instance.Enemies)
-        {
-            enemy.OnDeathEvent += GainHealth;
-        }
+      enemy.OnDeathEvent += GainHealth;
     }
+  }
 
-    public override void DeactivateEffect()
+  public override void DeactivateEffect()
+  {
+    base.DeactivateEffect();
+    foreach (MobController enemy in EnemiesManager.Instance.Enemies)
     {
-        base.DeactivateEffect();
-        foreach (MobController enemy in EnemiesManager.Instance.Enemies)
-        {
-            enemy.OnDeathEvent -= GainHealth;
-        }
+      enemy.OnDeathEvent -= GainHealth;
     }
+  }
 
-    public override void OnEnemyDeathApplyEffect(MobController enemy)
-    {
-        base.OnEnemyDeathApplyEffect(enemy);
-        enemy.OnDeathEvent += GainHealth;
-    }
+  public override void OnEnemyDeathApplyEffect(MobController enemy)
+  {
+    base.OnEnemyDeathApplyEffect(enemy);
+    enemy.OnDeathEvent += GainHealth;
+  }
 
-    private void GainHealth(Transform t = null)
-    {
-        _stats.UpdateCurrentHealth(healAmount);
-    }
+  private void GainHealth(Transform t = null)
+  {
+    _stats.UpdateCurrentHealth(healAmount);
+  }
 }
