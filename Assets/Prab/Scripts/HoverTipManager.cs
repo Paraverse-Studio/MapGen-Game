a342d48d32ff3cc99625fc337cd3139c02cec485
Abhishek Mohan <abhishekmohan.am@gmail.com>
2024-02-03 07:55:44 -0500

    start tip manager as disabled

--- Assets/Prab/Scripts/HoverTipManager.cs
+++ Assets/Prab/Scripts/HoverTipManager.cs
@@ -30,7 +30,7 @@ public class HoverTipManager : MonoBehaviour
   private void ShowTip(string tip, Vector2 mousePos)
   {
     tipText.text = tip;
-    tipWindow.sizeDelta = new Vector2(tipText.preferredWidth > 200 ? 200 : tipText.preferredWidth, tipText.preferredHeight);
+    tipWindow.sizeDelta = new Vector2(tipText.preferredWidth > 200 ? 200 : tipText.preferredWidth + 10, tipText.preferredHeight + 10);
 
     tipWindow.gameObject.SetActive(true);
     tipWindow.transform.position = new Vector2(mousePos.x, mousePos.y);
